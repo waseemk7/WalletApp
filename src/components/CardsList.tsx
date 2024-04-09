1a33bff6a069138d322cdb76003b57a9f0ddb60b
Waseem Kurne <kurne.waseem@gmail.com>
2024-04-09 15:56:50 +0530

    image fix

--- src/components/CardsList.tsx
+++ src/components/CardsList.tsx
@@ -1,7 +1,35 @@
-import {Text} from 'react-native';
+import React from 'react';
+import {Image, StyleSheet, View} from 'react-native';
 
 const CardsList = () => {
-  return <Text>Cards List</Text>;
+  const cards = [
+    require('../../assets/cards/Card_1.jpg'),
+    require('../../assets/cards/Card_2.jpg'),
+    require('../../assets/cards/Card_3.jpg'),
+    require('../../assets/cards/Card_4.jpg'),
+    require('../../assets/cards/Card_5.jpg'),
+    require('../../assets/cards/Card_6.jpg'),
+    require('../../assets/cards/Card_7.jpg'),
+    require('../../assets/cards/Card_8.jpg'),
+    require('../../assets/cards/Card_9.jpg'),
+  ];
+  return (
+    <View style={styles.container}>
+      <Image source={cards[0]} style={styles.image} />
+    </View>
+  );
 };
 
 export default CardsList;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 350, // Adjust as per your requirement
+    height: 200, // Adjust as per your requirement
+  },
+});
