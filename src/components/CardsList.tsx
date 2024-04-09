625120462af8000a6b5066a3d45c7293491de1d2
Waseem Kurne <kurne.waseem@gmail.com>
2024-04-09 16:29:27 +0530

    pan gestures start, end

--- src/components/CardsList.tsx
+++ src/components/CardsList.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
 import {Image, StyleSheet, View} from 'react-native';
+import {Gesture, GestureDetector} from 'react-native-gesture-handler';
+
+const cards = [
+  require('../../assets/cards/Card_1.jpg'),
+  require('../../assets/cards/Card_2.jpg'),
+  require('../../assets/cards/Card_3.jpg'),
+  require('../../assets/cards/Card_4.jpg'),
+  require('../../assets/cards/Card_5.jpg'),
+  require('../../assets/cards/Card_6.jpg'),
+  require('../../assets/cards/Card_7.jpg'),
+  require('../../assets/cards/Card_8.jpg'),
+  require('../../assets/cards/Card_9.jpg'),
+];
 
 const CardsList = () => {
-  const cards = [
-    require('../../assets/cards/Card_1.jpg'),
-    require('../../assets/cards/Card_2.jpg'),
-    require('../../assets/cards/Card_3.jpg'),
-    require('../../assets/cards/Card_4.jpg'),
-    require('../../assets/cards/Card_5.jpg'),
-    require('../../assets/cards/Card_6.jpg'),
-    require('../../assets/cards/Card_7.jpg'),
-    require('../../assets/cards/Card_8.jpg'),
-    require('../../assets/cards/Card_9.jpg'),
-  ];
+  const pan = Gesture.Pan()
+    .onStart(() => {
+      console.log('Panning started');
+    })
+    .onChange(() => {
+      console.log('Panning');
+    })
+    .onEnd(() => {
+      console.log('Panning ended');
+    });
+
   return (
-    <View style={styles.padding10}>
-      {cards.map((card, index) => (
-        <Image key={index} source={card} style={styles.image} />
-      ))}
-    </View>
+    <GestureDetector gesture={pan}>
+      <View style={styles.padding10}>
+        {cards.map((card, index) => (
+          <Image key={index} source={card} style={styles.image} />
+        ))}
+      </View>
+    </GestureDetector>
   );
 };
 
