9341e47d8d3537b6b46337a6adc68b307d1685f9
Waseem Kurne <kurne.waseem@gmail.com>
2024-04-09 14:58:20 +0530

    centre aligned

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <Text>App.tsx</Text>
     </SafeAreaView>
   );
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
